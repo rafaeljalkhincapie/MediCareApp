d7a48d701256f3f315ee5e5cef64d0786a3fb551
darwincruz <darwincruz.soto@gmail.com>
2025-10-04 10:45:37 +1000

    se cambió la p

--- app/(tabs)/Home.tsx
+++ app/(tabs)/Home.tsx
@@ -1,9 +1,8 @@
-import React from "react";
 import { Text, View } from "react-native";
 export default function Home() {
   return (
     <View>
-        <Text>Home Screen</Text>
+        <Text>Home Screen psdbgbgsfg s ssdfgbsdgbsfg  dghmfgmfgjm,fhj,fh,f</Text>
     </View>
   );
 }
